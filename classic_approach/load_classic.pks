f793d8db4c3de6b325804dc8fb095d731699fd6c
dandaw <daniel.dawidziuk@gmail.com>
2014-11-27 12:17:43 +0100

    Added procedure to load fact table

--- classic_approach/load_classic.pks
+++ classic_approach/load_classic.pks
@@ -2,5 +2,6 @@ CREATE OR REPLACE PACKAGE etl_perf.load_classic
 IS
   PROCEDURE load_bad;
   PROCEDURE load_stage;
+  PROCEDURE load_fact;
 END load_classic;
 /
--- perf_test.sql
+++ perf_test.sql
@@ -1,9 +1,8 @@
 DECLARE
   total_t TIMESTAMP := systimestamp;
   PROCEDURE exec_code(code VARCHAR2) IS
-    t TIMESTAMP;
+    t TIMESTAMP := systimestamp;
     BEGIN
-      t := systimestamp;
       EXECUTE IMMEDIATE 'BEGIN '||code||'; END;';
       DBMS_OUTPUT.PUT_LINE('Took: ' || (systimestamp-t) || ' to execute ' || code);
       COMMIT;
@@ -12,6 +11,8 @@ BEGIN
   exec_code('load_classic.load_stage');
 
   exec_code('load_classic.load_bad');
+
+  exec_code('load_classic.load_fact');
 
   DBMS_OUTPUT.PUT_LINE('Total took:'||(systimestamp-total_t));
 END;
